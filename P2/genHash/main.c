a6a2f78fafe5e50a106636f9a43d98e8e29be795
raonytog <raony.gomes@edu.ufes.br>
2024-07-27 11:22:20 -0300

    organizacao

--- P2/genHash/main.c
+++ P2/genHash/main.c
@@ -31,7 +31,7 @@ int main () {
     Aluno *aline = InitAluno("aline", 3);
     Aluno *gabriel = InitAluno("gabriel", 13);
 
-    /** Insere os alunos na tabela */
+    /** Insere os alunos na tabela por nome*/
     Hash *ht = InitHash(10);
     HashInsert(ht, "raony", raony, hf_nome);
     HashInsert(ht, "malu", malu, hf_nome);
@@ -39,15 +39,17 @@ int main () {
     HashInsert(ht, "eduardo", eduardo, hf_nome);
     HashInsert(ht, "marcela", marcela, hf_nome);
     HashInsert(ht, "aline", aline, hf_nome);
-    HashInsert(ht, 13, gabriel, hf_matricula);
 
-    /** Teste de sobreposicao de indice */
+    /** pois o criterio eh a matricula */
+    HashInsert(ht, 13, gabriel, hf_matricula); 
+
+    /** Teste de sobreposicao de indice (aline ao contrario) */
     Aluno *enila = InitAluno("enila", 4);
     HashInsert(ht, "enila", enila, hf_nome);
 
     /** 
      * Procura o aluno com o criterio do nome, 
-     * afinal, eh o que foi passado a ele 
+     * (o que foi passado a ele na hora de inserir na tabela) 
      */
     Aluno *encontrado = HashSearch(ht, "raony", hf_nome, ComparaNomeAlunos);
     if (encontrado) {
@@ -57,7 +59,8 @@ int main () {
     }
 
     /** 
-     * Procura o aluno com o criterio da matricula agora, 
+     * Procura o aluno com o criterio da matricula agora
+     * (mesma justificativa)
      */
     encontrado = HashSearch(ht, 13, hf_matricula, ComparaMatriculaAlunos);
     if (encontrado) {
@@ -66,18 +69,12 @@ int main () {
         printf("\n");
     }
 
-
     ImprimeHash(ht, PrintAluno);
-
     LiberaHash(ht);
-    LiberaAluno(raony);
-    LiberaAluno(malu);
-    LiberaAluno(barbara);
-    LiberaAluno(eduardo);
-    LiberaAluno(marcela);
-    LiberaAluno(aline);
-    LiberaAluno(gabriel);
-    LiberaAluno(enila);
+
+    LiberaAluno(raony);     LiberaAluno(malu);      LiberaAluno(barbara);
+    LiberaAluno(eduardo);   LiberaAluno(marcela);   LiberaAluno(aline);
+    LiberaAluno(gabriel);   LiberaAluno(enila);
 
     return 0;
 }
